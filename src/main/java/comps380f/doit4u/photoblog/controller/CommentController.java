f289e6c0b9bb4c77838ff862f61e6759c20c9f89
Kevin Li <lcpkevin0828@gmail.com>
2023-04-18 16:06:22 +0800

    Fixed the mapping problem of the 'create-comment'

--- src/main/java/comps380f/doit4u/photoblog/controller/CommentController.java
+++ src/main/java/comps380f/doit4u/photoblog/controller/CommentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/ticket")
+@RequestMapping("/comment")
 public class CommentController {
 
     @Autowired
