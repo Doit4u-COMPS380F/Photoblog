fb847ae5490bf3c27df125981f63c33d1f0a5015
timescam <timescam@duck.com>
2023-04-19 21:08:32 +0800

    photo not found for username

--- src/main/java/comps380f/doit4u/photoblog/exception/PhotoNotFound.java
+++ src/main/java/comps380f/doit4u/photoblog/exception/PhotoNotFound.java
@@ -4,4 +4,8 @@ public class PhotoNotFound extends Exception {
     public PhotoNotFound(long id) {
         super("Photo " + id + " does not exist.");
     }
+
+    public PhotoNotFound(String username) {
+        super("User: " + username + " have no photo(s).");
+    }
 }
